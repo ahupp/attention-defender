7ecc0be23ac6baacfc8bf0cc4a92d367545c6c2b
Adam Hupp <ahupp@oculus.com>
2015-12-02 13:36:42 -0800

    variable name, default include path

--- AttentionDefender.user.js
+++ AttentionDefender.user.js
@@ -2,9 +2,11 @@
 // @name         AttentionDefender
 // @namespace    http://hupp.org/adam/
 // @version      0.1
-// @description  Make selected domains load slowly.  If you're easily distracted this will give you a chance to decide if you really want to read it.
+// @description  Make selected domains load slowly.
 // @author       Adam Hupp <adam@hupp.org>
 // @grant        none
+// configured entirely through user includes, and tampermonkey defaults to "*" if you don't specify something
+// @include      http://hupp.org/adam/not-a-path
 // @run-at       document-body
 // ==/UserScript==
 /* jshint -W097 */
@@ -17,12 +19,13 @@
   }
 
   // state that says whether this session already hit this site
-  var key = "__was_delayed_" + window.location.hostname;
+  var hostname = window.location.hostname;
+  var key = "__was_delayed_" + hostname;
   if (window.sessionStorage[key]) {
     return;
   }
 
-  console.log("AttentionDefender: delaying", domain);
+  console.log("AttentionDefender: delaying", hostname);
   document.body.style.display = 'none';
   setTimeout(function() {
     document.body.style.display = '';
